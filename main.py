782328114c3ecad416acfc717e3abade5098e012
iamnikl <115219068+iamnikl@users.noreply.github.com>
2023-03-04 08:44:29 +0000

    deleted:    db.db 	modified:   main.py 	new file:   style/main.css 	new file:   style/main.css.map 	new file:   style/main.scss 	new file:   templates/index.html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,8 +12,8 @@ class Message(db.Model):
 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def startPage():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
